f9526ffc8d81f79a1491f731bf2dbeb12cb8408f
chanyoungcoding <white1614@naver.com>
2024-06-18 23:44:06 +0900

    기본설정 완료

--- src/server.js
+++ src/server.js
@@ -1,7 +1,25 @@
 import express from "express";
 
+// 새로고침 하지 않아도 자동으로 변경사항 반영
+import livereloadMiddleware from "connect-livereload";
+import livereload from "livereload";
+
 const app = express();
 
+const liveServer = livereload.createServer({
+  exts: ["js", "pug", "css"],
+  delay: 500,
+});
+
+liveServer.watch(__dirname);
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.use(livereloadMiddleware());
+app.get("/", (req, res) => res.render("home"));
+
+
 app.listen(3000, () => {
-  console.log("server listen")
-})
+  console.log(`Listening on http://localhost:3000`)
+});
